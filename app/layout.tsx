582e55bde60e86626c5fe38e9f8e83b13b476610
Vinayak Anvekar <fairvinay@gmail.com>
2025-06-16 03:53:15 +0530

    added layout.tsx Script from next/script suggested in https://tagmanager.google.com/#/admin/accounts/6299387399/containers/222405349/install

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import {API} from '@/libs/client';
 import {ThemeProvider} from "@/providers/ThemeProvider";
 import { EquityReduxProvider } from '@/providers/EquityReduxProvider';
 import Analytics from './analytics/Analytics';
-
+import Script from "next/script"
 const inter = Inter({subsets: ['latin']})
 
 
@@ -26,15 +26,19 @@ export default function RootLayout({
         <html lang="en" className=''>
         <title>Store Notify Stocks</title>
         <head>{/* Google Tag Manager */}
-        <script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PDF5CBRC');
-          `}
-        </script>
+         <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', '${GTM_ID}');
+  `,
+        }}
+      />
 	   </head>
         <link rel="icon" href="/favicon.ico" sizes="any"/>
         <body className={inter.className}>
